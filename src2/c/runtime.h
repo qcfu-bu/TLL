30a33a6ec43e289a67bba7dc9144e8883b431ba1
qcfu <qcfu@bu.edu>
2023-04-09 15:20:19 -0400

    tll

--- src2/c/runtime.h
+++ src2/c/runtime.h
@@ -24,17 +24,16 @@ typedef _tll_node *tll_node;
 #define tll_clo_size sizeof(_tll_clo)
 #define tll_node_size sizeof(_tll_node)
 
-tll_ptr proc_sender(tll_ptr x, tll_env env);
 tll_ptr proc_stdout(tll_ptr ch);
 tll_ptr proc_stdin(tll_ptr ch);
 tll_ptr proc_stderr(tll_ptr ch);
 
 void instr_init();
-void instr_clo(tll_ptr *x, tll_ptr (*f)(tll_ptr, tll_env), int narg, ...);
+void instr_clo(tll_ptr *x, tll_ptr (*f)(tll_ptr, tll_env), int size, ...);
 void instr_app(tll_ptr *x, tll_ptr clo, tll_ptr v);
 void instr_struct(tll_ptr *x, int tag, int size, ...);
 void instr_open(tll_ptr *x, tll_ptr (*f)(tll_ptr));
-void instr_fork(tll_ptr *x, tll_ptr (*f)(tll_env), int narg, ...);
+void instr_fork(tll_ptr *x, tll_ptr (*f)(tll_env), int size, ...);
 void instr_recv(tll_ptr *x, tll_ptr ch);
 void instr_send(tll_ptr *x, tll_ptr ch, tll_ptr msg);
 void instr_close(tll_ptr *x, tll_ptr ch);
