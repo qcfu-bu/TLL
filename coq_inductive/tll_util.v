6eb890df8d02e3c61a5a19cd2aaee259d07aa3f7
qcfu-bu <qcfu@bu.edu>
2023-06-19 01:19:11 -0400

    coq-inductive

--- coq_inductive/tll_util.v
+++ coq_inductive/tll_util.v
@@ -13,34 +13,47 @@ Inductive nth A : nat -> list A -> A -> Prop :=
   nth n ls m ->
   nth n.+1 (m' :: ls) m.
 
-Inductive All1 A (P : A -> Prop) : list A -> Prop :=
-| All1_nil : All1 P nil
-| All1_cons m ls : P m -> All1 P ls -> All1 P (m :: ls).
+Inductive Forall1 A (P : A -> Prop) : list A -> Prop :=
+| Forall1_nil : Forall1 P nil
+| Froall1_cons m ls : P m -> Forall1 P ls -> Forall1 P (m :: ls).
 
-Inductive One2 A R : list A -> list A -> Prop :=
-| One2_hd m m' ls :
+Inductive Exists1 A (P : A -> Prop) : list A -> Prop :=
+| Exists1_exist m ls : 
+  P m ->
+  Exists1 P (m :: ls)
+| Exists1_extend m ls :
+  Exists1 P ls ->
+  Exists1 P (m :: ls).
+
+Inductive Forall2 A (R : A -> A -> Prop) : list A -> list A -> Prop :=
+| Forall2_nil :
+  Forall2 R nil nil
+| Froall2_cons m m' ls ls' :
   R m m' ->
-  One2 R (m :: ls) (m' :: ls)
-| One2_tl m ls ls' :
-  One2 R ls ls' ->
-  One2 R (m :: ls) (m :: ls').
+  Forall2 R ls ls' ->
+  Forall2 R (m :: ls) (m' :: ls').
 
-Inductive All2 A (R : A -> A -> Prop) : list A -> list A -> Prop :=
-| All2_nil : All2 R nil nil
-| All2_cons m m' ls ls' :
+Inductive Exists2 A (R : A -> A -> Prop) : list A -> list A -> Prop :=
+| Exists2_exist m m' ls :
   R m m' ->
-  All2 R ls ls' ->
-  All2 R (m :: ls) (m' :: ls').
+  Exists2 R (m :: ls) (m' :: ls)
+| Exists2_extend m ls ls' :
+  Exists2 R ls ls' ->
+  Exists2 R (m :: ls) (m :: ls').
 
-Inductive All2i A R : nat -> list A -> list A -> Prop :=
-| All2i_nil i : All2i R i nil nil
-| All2i_cons i m m' ls ls' :
+Inductive Forall2i' A (R : nat -> A -> A -> Prop) : nat -> list A -> list A -> Prop :=
+| Forall2i'_nil i :
+  Forall2i' R i nil nil
+| Forall2i'_cons i m m' ls ls' :
   R i m m' ->
-  All2i R i.+1 ls ls' ->
-  All2i R i (m :: ls) (m' :: ls').
+  Forall2i' R i.+1 ls ls' ->
+  Forall2i' R i (m :: ls) (m' :: ls').
 
-Lemma nth_All2 A (R : A -> A -> Prop) xs ys x n :
-  All2 R xs ys -> nth n xs x -> exists y, nth n ys y /\ R x y.
+Definition Forall2i A R ls ls' :=
+  @Forall2i' A R 0 ls ls'.
+
+Lemma nth_Forall2 A (R : A -> A -> Prop) xs ys x n :
+  Forall2 R xs ys -> nth n xs x -> exists y, nth n ys y /\ R x y.
 Proof with eauto using nth.
   move=>h. elim: h x n=>{xs ys}.
   move=>x n h. inv h.
@@ -49,8 +62,8 @@ Proof with eauto using nth.
   have[y[h1 h2]]:=ih _ _ H3. exists y...
 Qed.
 
-Lemma nth_All2i A (R : nat -> A -> A -> Prop) xs ys x i n :
-  All2i R i xs ys -> nth n xs x -> exists y, nth n ys y /\ R (i + n) x y.
+Lemma nth_Forall2i' A (R : nat -> A -> A -> Prop) xs ys x i n :
+  Forall2i' R i xs ys -> nth n xs x -> exists y, nth n ys y /\ R (i + n) x y.
 Proof with eauto using nth.
   move=>h. elim: h x n=>{xs ys i}.
   move=>i x n h. inv h.
@@ -58,4 +71,12 @@ Proof with eauto using nth.
   exists m'. split... replace (i + 0) with i by lia...
   have[y[h4 h5]]:=ih _ _ H3.
   exists y. split... replace (i + n0.+1) with (i.+1 + n0) by lia...
+Qed.
+
+Lemma nth_Forall2i A (R : nat -> A -> A -> Prop) xs ys x n :
+  Forall2i R xs ys -> nth n xs x -> exists y, nth n ys y /\ R n x y.
+Proof with eauto using nth.
+  move=>h1 h2. unfold Forall2i in h1.
+  have[y[h3 h4]]:=nth_Forall2i' h1 h2.
+  exists y=>//.
 Qed.
