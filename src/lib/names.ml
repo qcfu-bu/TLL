01b2ab66d1c05401a9aaeae7239db23983fd48b4
qcfu <qcfu@bu.edu>
2023-07-22 18:13:29 -0400

    tll

--- src/lib/names.ml
+++ src/lib/names.ml
@@ -1,135 +1,41 @@
 open Fmt
 
-(* meta variables *)
-module M : sig
-  type t
+module Name : sig
+  module Make () : sig
+    type t
 
-  val mk : unit -> t
-  val equal : t -> t -> bool
-  val compare : t -> t -> int
-  val pp : Format.formatter -> t -> unit
-  val to_string : t -> string
+    module Set : Set.S with type elt = t
+    module Map : Map.S with type key = t
+
+    val mk : string -> t
+    val equal : t -> t -> bool
+    val compare : t -> t -> int
+    val pp : Format.formatter -> t -> unit
+  end
 end = struct
-  type t = int
+  module Make () = struct
+    module Inner = struct
+      type t = string * int
 
-  let stamp = ref 0
+      let stamp = ref 0
 
-  let mk () =
-    incr stamp;
-    !stamp
+      let mk s =
+        incr stamp;
+        (s, !stamp)
 
-  let equal x y = x = y
-  let compare x y = Int.compare x y
-  let pp fmt id = pf fmt "??%d" id
-  let to_string x = to_to_string pp x
+      let equal x y = snd x = snd y
+      let compare x y = Int.compare (snd x) (snd y)
+      let pp fmt (s, id) = pf fmt "%s_%d" s id
+    end
+
+    include Inner
+    module Set = Set.Make (Inner)
+    module Map = Map.Make (Inner)
+  end
 end
 
-(* constant identifiers *)
-module I : sig
-  type t
-
-  val mk : string -> t
-  val equal : t -> t -> bool
-  val compare : t -> t -> int
-  val extend : t -> string -> t
-  val pp : Format.formatter -> t -> unit
-  val to_string : t -> string
-  val is_main : t -> bool
-end = struct
-  type t = string * int
-
-  let stamp = ref 0
-
-  let mk s =
-    incr stamp;
-    (s, !stamp)
-
-  let equal x y = snd x = snd y
-  let compare x y = Int.compare (snd x) (snd y)
-
-  let extend (s0, _) s1 =
-    incr stamp;
-    (s0 ^ s1, !stamp)
-
-  let pp fmt (s, id) = pf fmt "%s_i%d" s id
-  let to_string x = to_to_string pp x
-  let is_main (s, _) = s = "main"
-end
-
-(* data identifiers *)
-module D : sig
-  type t
-
-  val mk : string -> t
-  val equal : t -> t -> bool
-  val compare : t -> t -> int
-  val extend : t -> string -> t
-  val pp : Format.formatter -> t -> unit
-  val to_string : t -> string
-end = struct
-  type t = string * int
-
-  let stamp = ref 0
-
-  let mk s =
-    incr stamp;
-    (s, !stamp)
-
-  let equal x y = snd x = snd y
-  let compare x y = Int.compare (snd x) (snd y)
-
-  let extend (s0, _) s1 =
-    incr stamp;
-    (s0 ^ s1, !stamp)
-
-  let pp fmt (s, id) = pf fmt "%s_d%d" s id
-  let to_string x = to_to_string pp x
-end
-
-(* constructor identifiers *)
-module C : sig
-  type t
-
-  val mk : string -> t
-  val equal : t -> t -> bool
-  val compare : t -> t -> int
-  val get_id : t -> int
-  val get_name : t -> string
-  val extend : t -> string -> t
-  val pp : Format.formatter -> t -> unit
-  val to_string : t -> string
-end = struct
-  type t = string * int
-
-  let stamp = ref 0
-
-  let mk s =
-    incr stamp;
-    (s, !stamp)
-
-  let equal x y = snd x = snd y
-  let compare x y = Int.compare (snd x) (snd y)
-  let get_id (_, id) = id
-  let get_name (s, _) = s
-
-  let extend (s0, _) s1 =
-    incr stamp;
-    (s0 ^ s1, !stamp)
-
-  let pp fmt (s, id) = pf fmt "%s_c%d" s id
-  let to_string x = to_to_string pp x
-end
-
-(* sets *)
-module SSet = Set.Make (String)
-module MSet = Set.Make (M)
-module ISet = Set.Make (I)
-module CSet = Set.Make (C)
-module DSet = Set.Make (D)
-
-(* maps *)
-module SMap = Map.Make (String)
-module MMap = Map.Make (M)
-module IMap = Map.Make (I)
-module CMap = Map.Make (C)
-module DMap = Map.Make (D)
+module Meta = Name.Make ()
+module Ind = Name.Make ()
+module Constr = Name.Make ()
+module Proj = Name.Make ()
+module Method = Name.Make ()
---
+++ src/lib/syntax2_util.ml
@@ -0,0 +1,236 @@
+open Bindlib
+open Names
+open Syntax2
+
+module SVar = struct
+  module Inner = struct
+    open Fmt
+
+    type t = sort var
+
+    let mk = new_var (fun x -> SVar x)
+    let compare = compare_vars
+    let pp fmt x = pf fmt "%s_s%d" (name_of x) (uid_of x)
+  end
+
+  include Inner
+  module Set = Set.Make (Inner)
+  module Map = Map.Make (Inner)
+end
+
+module Var = struct
+  module Inner = struct
+    open Fmt
+
+    type t = tm var
+
+    let mk = new_var (fun x -> SVar x)
+    let compare = compare_vars
+    let pp fmt x = pf fmt "%s_s%d" (name_of x) (uid_of x)
+  end
+
+  include Inner
+  module Set = Set.Make (Inner)
+  module Map = Map.Make (Inner)
+end
+
+(* spine forms *)
+let mkApps h ms = Array.fold_left (fun acc m -> App (acc, m)) h ms
+
+let rec unApps m =
+  match m with
+  | App (m, n) ->
+    let h, ms = unApps m in
+    (h, Array.append ms [| n |])
+  | _ -> (m, [||])
+
+(* smart constructors *)
+let var x = Var x
+
+(* sort *)
+let _U = box U
+let _L = box L
+let _SVar = box_var
+let _SMeta x = box_apply (fun ss -> SMeta (x, ss))
+
+(* rel *)
+let _N = box N
+let _R = box R
+
+(* inference *)
+let _Ann = box_apply2 (fun m a -> Ann (m, a))
+let _IMeta x = box_apply2 (fun ss ms -> IMeta (x, ss, ms))
+let _TMeta x = box_apply2 (fun ss ms -> TMeta (x, ss, ms))
+let _PMeta x = box (PMeta x)
+
+(* core *)
+let _Type = box_apply (fun s -> Type s)
+let _Var = box_var
+let _Pi rel = box_apply3 (fun s a b -> Pi (rel, s, a, b))
+let _Lam rel = box_apply3 (fun s a m -> Lam (rel, s, a, m))
+let _Fix = box_apply2 (fun a m -> Fix (a, m))
+let _App = box_apply2 (fun m n -> App (m, n))
+let _Let rel = box_apply2 (fun m n -> Let (rel, m, n))
+
+(* inductive *)
+let _Ind ind = box_apply (fun ss -> Ind (ind, ss))
+let _Constr constr = box_apply (fun ss -> Constr (constr, ss))
+let _Absurd = box Absurd
+let _Match = box_apply3 (fun ms a cls -> Match (ms, a, cls))
+
+(* record *)
+let _Record = box_apply2 (fun s mp -> Record (s, mp))
+let _Struct = box_apply2 (fun s mp -> Struct (s, mp))
+let _Proj prj = box_apply (fun m -> Proj (prj, m))
+
+(* bound pattern *)
+let _P0Rel = box P0Rel
+let _P0Constr constr = box_apply (fun ps -> P0Constr (constr, ps))
+
+(* box *)
+let box_proj_map lift map =
+  Proj.Map.fold
+    (fun proj a map_box ->
+      let a_box = lift a in
+      box_apply2 (fun a map -> Proj.Map.add proj a map) a_box map_box)
+    map (box Proj.Map.empty)
+
+let box_rel = function
+  | N -> _N
+  | R -> _R
+
+let rec box_p0 = function
+  | P0Rel -> _P0Rel
+  | P0Constr (constr, ps) ->
+    let ps = Array.map box_p0 ps in
+    _P0Constr constr (box_array ps)
+
+(* lifting *)
+let rec lift_sort = function
+  | U -> _U
+  | L -> _L
+  | SVar x -> _SVar x
+  | SMeta (x, ss) ->
+    let ss = Array.map lift_sort ss in
+    _SMeta x (box_array ss)
+
+let rec lift_tm = function
+  (* inference *)
+  | Ann (m, a) -> _Ann (lift_tm m) (lift_tm a)
+  | IMeta (x, ss, ms) ->
+    let ss = Array.map lift_sort ss in
+    let ms = Array.map lift_tm ms in
+    _IMeta x (box_array ss) (box_array ms)
+  | TMeta (x, ss, ms) ->
+    let ss = Array.map lift_sort ss in
+    let ms = Array.map lift_tm ms in
+    _TMeta x (box_array ss) (box_array ms)
+  | PMeta x -> _PMeta x
+  (* core *)
+  | Type s -> _Type (lift_sort s)
+  | Var x -> _Var x
+  | Pi (rel, s, a, bnd) ->
+    _Pi rel (lift_sort s) (lift_tm a) (box_binder lift_tm bnd)
+  | Lam (rel, s, a, bnd) ->
+    _Lam rel (lift_sort s) (lift_tm a) (box_binder lift_tm bnd)
+  | Fix (a, bnd) -> _Fix (lift_tm a) (box_binder lift_tm bnd)
+  | App (m, n) -> _App (lift_tm m) (lift_tm n)
+  | Let (rel, m, bnd) -> _Let rel (lift_tm m) (box_binder lift_tm bnd)
+  (* inductive *)
+  | Ind (ind, ss) ->
+    let ss = Array.map lift_sort ss in
+    _Ind ind (box_array ss)
+  | Constr (constr, ss) ->
+    let ss = Array.map lift_sort ss in
+    _Constr constr (box_array ss)
+  | Match (ms, a, cls) ->
+    let ms =
+      Array.map (fun (m, rel) -> box_pair (lift_tm m) (box_rel rel)) ms
+    in
+    let cls =
+      Array.map
+        (fun (p0s, mbnd) ->
+          let p0s = Array.map box_p0 p0s in
+          let mbnd = box_mbinder lift_tm mbnd in
+          box_pair (box_array p0s) mbnd)
+        cls
+    in
+    _Match (box_array ms) (lift_tm a) (box_array cls)
+  | Absurd -> _Absurd
+  (* record *)
+  | Record (s, map) -> _Record (lift_sort s) (box_proj_map lift_tm map)
+  | Struct (s, map) -> _Record (lift_sort s) (box_proj_map lift_tm map)
+  | Proj (proj, m) -> _Proj proj (lift_tm m)
+
+(* pattern equality *)
+let rec equal_p0 p1 p2 =
+  match (p1, p2) with
+  | P0Rel, P0Rel -> true
+  | P0Constr (constr1, p0s1), P0Constr (constr2, p0s2) ->
+    Constr.equal constr1 constr1 && equal_p0s p0s1 p0s2
+  | _ -> false
+
+and equal_p0s ps1 ps2 = Array.for_all2 equal_p0 ps1 ps2
+
+and equal_pbinder eq (p0s1, bnd1) (p0s2, bnd2) =
+  equal_p0s p0s1 p0s2 && eq_mbinder eq bnd1 bnd2
+
+(* pattern binding *)
+let rec mvar_of_p p =
+  match p with
+  | PVar x -> ([| x |], P0Rel)
+  | PConstr (constr, ps) ->
+    let xs, p0s = mvar_of_ps ps in
+    (xs, P0Constr (constr, p0s))
+
+and mvar_of_ps ps =
+  Array.fold_left_map
+    (fun acc p ->
+      let xs, p0 = mvar_of_p p in
+      (Array.append acc xs, p0))
+    [||] ps
+
+let rec p_of_mvar mvar p0 =
+  match p0 with
+  | P0Rel ->
+    let x = mvar.(0) in
+    let mvar = Array.sub mvar 1 (Array.length mvar - 1) in
+    (mvar, PVar x)
+  | P0Constr (constr, p0s) ->
+    let mvar, p0s = ps_of_mvar mvar p0s in
+    (mvar, PConstr (constr, p0s))
+
+and ps_of_mvar mvar p0s = Array.fold_left_map p_of_mvar mvar p0s
+
+let bind_ps ps m =
+  let xs, p0s = mvar_of_ps ps in
+  let bnd = bind_mvar xs m in
+  box_apply (fun bnd -> (p0s, bnd)) bnd
+
+let unbind_ps (p0s, bnd) =
+  let xs, m = unmbind bnd in
+  let _, ps = ps_of_mvar xs p0s in
+  (ps, m)
+
+let unbind_ps2 (p0s1, bnd1) (p0s2, bnd2) =
+  assert (equal_p0s p0s1 p0s2);
+  let xs, m1, m2 = unmbind2 bnd1 bnd2 in
+  let _, ps = ps_of_mvar xs p0s1 in
+  (ps, m1, m2)
+
+(* pattern substitution *)
+let psubst (p0s, bnd) ms =
+  let rec match_p0 p0 m =
+    match (p0, unApps m) with
+    | P0Rel, _ -> [| m |]
+    | P0Constr (constr1, p0s), (Constr (constr2, _), ms)
+      when Constr.equal constr1 constr2 ->
+      match_p0s p0s ms
+    | _ -> failwith "match_p0"
+  and match_p0s p0s ms =
+    Array.fold_left
+      (fun acc (p0, m) -> Array.append acc (match_p0 p0 m))
+      [||] (Array.combine p0s ms)
+  in
+  let ms = match_p0s p0s ms in
+  msubst bnd ms
