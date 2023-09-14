58476953dcc127f6c6023871f55b95c25838bc52
qcfu <qcfu@bu.edu>
2023-09-14 13:17:17 -0400

    tll

--- src/gen/prelude.h
+++ src/gen/prelude.h
@@ -1,9 +1,9 @@
 #ifndef PRELUDE_H
 #define PRELUDE_H
 
-#define __exU__ 34
-#define __exL__ 32
-#define __tt__ 19
+#define __exU__ 41
+#define __exL__ 39
+#define __tt__ 22
 #define __true__ 4
 #define __false__ 5
 
